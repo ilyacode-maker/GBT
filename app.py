a23ce2c341d0c8a7071958e8886026f9f7d24665
Website709 <earthquakelist709@gmail.com>
2024-04-30 15:37:14 +0100

    Audio Stream Implemented

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, send_file
+import time
 
 app = Flask(__name__)
 
 @app.route('/',methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        message  = request.form['prompt']
+        message  = request.get_json()["Chunk"]
         return render_template('index.html', res=message)
     else:
         return render_template('index.html')
+
+@app.route("/audio", methods = ['GET','POST'])
+def handleAudio():
+    CHUNK_SIZE = 1024
+    END_SIGNATURE = b'\x01\x02\x03\x04\x05\x06\x07\x08'
+    AudioFiles = ['./out002.mp3','./out003.mp3','./out004.mp3','./out005.mp3','./out006.mp3','./out007.mp3']
+    def generate():
+        time.sleep(1)
+        i = 0
+        while i < len(AudioFiles):
+            file = open(AudioFiles[i],'rb');
+            i += 1
+            while True:
+                file_chunk = file.read(CHUNK_SIZE)
+                if not file_chunk:
+                    yield END_SIGNATURE
+                    time.sleep(.2)
+                    break;
+                yield file_chunk
+            file.close();
+
+    
+    return Response(generate(), mimetype="application/octet-stream")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -4,12 +4,11 @@ const url = window.location + "audio";
 //====================Variables==================//
 var name = $("#BookName").val();
 var tone = $("#prompt").val();
+const END_SIGNATURE = [1,2,3,4,5,6,7,8]
 var audio1 = new Audio("./static/out000.mp3");
 var audio2 = new Audio("./static/out001.mp3");
-var audio3 = new Audio("./static/out002.mp3");
-var audio4 = new Audio("./static/out003.mp3");
-var audio5 = new Audio("./static/out004.mp3");
-var Audios = [audio1, audio2, audio3];
+var Audios = [audio1, audio2];
+
 
 
 
@@ -23,39 +22,62 @@ TODO LIST/:
 
 */
 
-//======Given a Name Only=============//
+//====== Handling Ebook Input =============//
 
-function GetAudioName(BookName,tone) {
+// var book = ePub();
+// var rendition;
 
-  FirstAudio = SendChunk(BookName,tone,url);
-  var index = 0;
-  Audios.push(FirstAudio);
-  Audios[0].play();
-  LiveAudio(0);
 
-}
+// document.getElementById("FileInput").addEventListener("change", function(e) {
+//   var file = e.target.files[0];
+//   if (window.FileReader) {
+
+//     var reader = new FileReader();
+//     reader.onload = openBook;
+//     reader.readAsArrayBuffer(file);
+
+//   }
+// });
+
+
+
+// function openBook(e){
+//   var bookData = e.target.result;
+
+//   book.open(bookData, "binary");
+
+//   rendition = book.renderTo("area", {
+//     width: "100%",
+//     height: 600
+//   });
+
+//   rendition.display();
+
+// }
 
 
 
 //======Simulating a late response======//
-setTimeout(() => {
+// setTimeout(() => {
 
-  Audios.push(audio4);
+//   Audios.push(audio4);
 
-},2000);
+// },2000);
 
-setTimeout(() => {
+// setTimeout(() => {
 
-  Audios.push(audio5);
+//   Audios.push(audio5);
 
-},5000);
+// },5000);
 
-setTimeout(() => {
+// setTimeout(() => {
 
-  SendChunk("Something","who_cares", url);
+//   SendChunk("Something","who_cares", url);
 
-}, 7500);
+// }, 7500);
 //====Piecing Audio Chunks together====// 
+
+
 
 Audios[0].play();
 LiveAudio(0);
@@ -77,24 +99,93 @@ function LiveAudio(index) {
 
 
 //========== Sending Request to get Audio Chunk in return =============//
-async function SendChunk(chunk,tone, url) {
-  try {
-    data  = {"Chunk" : chunk};
-    var response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-      });
-      var audioBlob = await response.blob();
-      var AudioURL = URL.createObjectURL(audioBlob);
-      var AudioObject = new Audio(AudioURL);
-      Audios.push(AudioObject);
-  } catch (error) {
+// async function SendChunk(chunk,tone, url) {
+//   try {
+//     data  = {"Chunk" : chunk};
+//     var response = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data)
+//       });
+//       console.log(response);
+//       var audioBlob = await response.blob();
+//       var AudioURL = URL.createObjectURL(audioBlob);
+//       var AudioObject = new Audio(AudioURL);
+//       Audios.push(AudioObject);
+//   } catch (error) {
 
-    console.log("error : " + error);
+//     console.log("error : " + error);
+
+//   }
+  
+// }
+
+// SendChunk("something","who_cares",url)
+
+
+
+function EqualList(list1,list2) {
+
+  if (list1.length != list2.length) {
+
+    return false;
 
   }
-  
+  var Different = false
+  for (var i = 0; i < list1.length;i++) {
+
+    if (list1[i] != list2[i]) {
+
+      Different = true;
+      break;
+
+    }
+
+  }
+  return !Different
+
 }
+
+async function consumeStream(reader) {
+  chunks = [];
+  while (true) {
+      var { done, value } = await reader.read();
+      if (done) {
+          console.log("Stream is done");
+          break;
+      }
+      console.log(value);
+      LastEight = value.slice(-8);
+      value = value.slice(0, value.length - 8);
+      chunks.push(value); 
+      if (EqualList(LastEight,END_SIGNATURE)) {
+
+        var arr = new Uint8Array(chunks.reduce((acc, chunks) => acc.concat(Array.from(chunks)), []));
+        var audioBlob = new Blob([arr]);
+        var AudioURL = URL.createObjectURL(audioBlob);
+        var AudioObject = new Audio(AudioURL);
+        Audios.push(AudioObject);
+        chunks = [];
+
+      }else {
+
+        chunks.push(LastEight)
+
+      }
+      
+      
+
+      
+      
+      
+  }
+}
+
+fetch(url)
+    .then(response => response.body)
+    .then(stream => {
+        var reader = stream.getReader();
+      consumeStream(reader);
+    });
